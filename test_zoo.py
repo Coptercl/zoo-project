c6c5d8e0f719d71956ea3a82e7c74c8875c84aea
Coptercl <peerachetkraivijitr@gmail.com>
2024-11-26 07:56:01 +0000

    3429

--- test_zoo.py
+++ test_zoo.py
@@ -7,6 +7,18 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(30), 150)
+
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(65), 100)
+
+    def test_invalid_age(self):
+        self.assertEqual(self.zoo.get_ticket_price(-5), 0)
        
     # Add your additional test cases here.
 
